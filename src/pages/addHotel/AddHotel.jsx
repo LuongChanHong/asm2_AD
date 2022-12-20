289d2e0a5eb2e23a161c021a22a137a5134ff2d0
honglc <hongmainmail@gmail.com>
2022-12-20 18:03:41 +0700

    addHotel comp error

--- src/pages/addHotel/AddHotel.jsx
+++ src/pages/addHotel/AddHotel.jsx
@@ -21,8 +21,19 @@ const AddHotel = () => {
     price: 100,
     image: "",
     featured: "yes",
-    rooms: [],
+    room: [],
   });
+  const [inputValid, setInputValid] = useState({
+    name: true,
+    address: true,
+    distance: true,
+    title: true,
+    room: true,
+    description: true,
+    price: true,
+    image: true,
+  });
+  const [isInputsValid, setInputsValid] = useState(true);
   const [roomList, setRoomList] = useState([]);
 
   const hotelTypeList = useSelector((state) => state.hotel.types);
@@ -35,7 +46,7 @@ const AddHotel = () => {
   useEffect(() => {
     const getAllRoom = async () => {
       const response = await get("/get-all-room");
-      console.log("response.data:", response.data);
+      // console.log("response.data:", response.data);
       if (response) {
         setRoomList(response.data);
       }
@@ -43,43 +54,43 @@ const AddHotel = () => {
     getAllRoom();
   }, []);
 
-  const handleEvent = (event) => {
-    // console.log("event:", event.target);
-    const target = event.target;
-    if (target.type === "checkbox") {
-      return {
-        value: target.value,
-        checked: target.checked,
-      };
-    } else {
-      return {
-        name: target.name,
-        value: target.value,
-      };
-    }
-  };
+  // ===================================================================
+  // RENDER - START
+  // ===================================================================
 
-  const handleChange = (event) => {
-    const { name, value } = handleEvent(event);
-    setInput({ ...input, [name]: value });
-  };
-
-  const handleAddHotel = (event) => {
-    event.preventDefault();
-    console.log("input:", input);
-  };
-
-  const renderInput = (label, name, value, type) => {
+  const renderCharInput = (label, name, value) => {
+    const type = typeof value === "string" ? "text" : "number";
+    // console.log(inputValid[name]);
     return (
-      <div className="addHotel__form--item  align-items-center">
-        <span>{label}:</span>
+      <div className="addHotel__form--item align-items-center">
+        <span className={`${inputValid[name] ? `` : `warning-text`}`}>
+          {label}:
+        </span>
         <input
-          className="addHotel__form--input input-underline input-outline-none"
+          className={`addHotel__form--input ${
+            inputValid[name] ? `` : `input-warning`
+          } input-underline input-outline-none`}
           type={type}
           name={name}
           value={value}
           onChange={handleChange}
         />
+      </div>
+    );
+  };
+
+  const renderTextArea = (label, name, value) => {
+    return (
+      <div className="addHotel__form--item w-100 align-items-center">
+        <p className={`${inputValid.image ? `` : `warning-text`}`}>{label}:</p>
+        <textarea
+          className="w-100 input-outline-none"
+          name={name}
+          value={value}
+          onChange={handleChange}
+          rows="4"
+          // cols="50"
+        ></textarea>
       </div>
     );
   };
@@ -104,6 +115,96 @@ const AddHotel = () => {
     );
   };
 
+  // ===================================================================
+  // RENDER - END
+  // ===================================================================
+
+  // ===================================================================
+  // HANDLER - START
+  // ===================================================================
+
+  const handleEvent = (event) => {
+    // console.log("event:", event.target);
+    const target = event.target;
+    if (target.type === "checkbox") {
+      return {
+        name: target.name,
+        value: target.value,
+        type: target.type,
+        checked: target.checked,
+      };
+    } else {
+      return {
+        name: target.name,
+        value: target.value,
+        type: target.type,
+      };
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value, checked, type } = handleEvent(event);
+
+    if (type === "checkbox") {
+      const selectedCheckBox = checked
+        ? [...input[name], value]
+        : input[name].filter((item) => item != value);
+      setInput({
+        ...input,
+        [name]: selectedCheckBox,
+      });
+    } else {
+      setInput({ ...input, [name]: value });
+    }
+
+    console.log("input:", input);
+  };
+
+  const arrayValidation = (name, value) => {
+    const isValid = value.length !== 0 ? true : false;
+    setInputValid({ ...inputValid, [name]: isValid });
+    return isValid;
+  };
+
+  const textValidation = (name, value) => {
+    const isValid = value !== "" ? true : false;
+    setInputValid({ ...inputValid, [name]: isValid });
+    return isValid;
+  };
+
+  const numberValidation = (name, value) => {
+    const isValid = value !== 0 ? true : false;
+    setInputValid({ ...inputValid, [name]: isValid });
+    return isValid;
+  };
+
+  const inputValidation = () => {
+    let isAllValid = true;
+    for (const attri in input) {
+      if (typeof input[attri] === "string") {
+        isAllValid &= textValidation(attri, input[attri]);
+      }
+      if (typeof input[attri] === "number") {
+        isAllValid &= numberValidation(attri, input[attri]);
+      }
+      if (typeof input[attri] === "object") {
+        isAllValid &= arrayValidation(attri, input[attri]);
+      }
+    }
+    setInputsValid(isAllValid);
+  };
+
+  const handleAddHotel = (event) => {
+    event.preventDefault();
+
+    inputValidation();
+    console.log(inputValid);
+  };
+
+  // ===================================================================
+  // HANDLER - END
+  // ===================================================================
+
   return (
     <Wrapper>
       <section className="addHotel__container py-3">
@@ -114,8 +215,8 @@ const AddHotel = () => {
               <form action="">
                 <div className="row">
                   <div className="col-md-12 col-lg-6">
-                    {renderInput("Name", "name", input.name, "text")}
-                    {renderInput("Address", "address", input.address, "text")}
+                    {renderCharInput("Name", "name", input.name)}
+                    {renderCharInput("Address", "address", input.address)}
                     <div className="row">
                       <div className="col-md-12 col-lg-4 py-md-2">
                         {renderOptionInput("Type", "type", hotelTypeList)}
@@ -134,30 +235,31 @@ const AddHotel = () => {
 
                     <div className="row">
                       <div className="col-md-12 col-lg-6">
-                        {renderInput(
+                        {renderCharInput(
                           "Distance from city center",
                           "distance",
-                          input.distance,
-                          "number"
+                          input.distance
                         )}
                       </div>
                       <div className="col-md-12 col-lg-6">
-                        {renderInput("Price", "price", input.price, "number")}
+                        {renderCharInput("Price", "price", input.price)}
                       </div>
                     </div>
-                    {renderInput("Title", "title", input.title, "text")}
-                    {renderInput(
+                    {renderCharInput("Title", "title", input.title)}
+                    {renderCharInput(
                       "Desciption",
-                      "desciption",
-                      input.description,
-                      "text"
+                      "description",
+                      input.description
                     )}
-
-                    {renderInput("Image Link", "image", input.image, "text")}
+                    {renderTextArea("Image Link", "image", input.image)}
                   </div>
 
                   <div className="col-md-12 col-lg-6">
-                    <span>Room List</span>
+                    <span
+                      className={`${inputValid.room ? `` : `warning-text`}`}
+                    >
+                      Room List
+                    </span>
                     <Table striped>
                       <thead>
                         <tr>
@@ -170,13 +272,18 @@ const AddHotel = () => {
                       </thead>
                       <tbody>
                         {roomList.map((room, i) => (
-                          <tr>
+                          <tr key={i}>
                             <td>{++i}</td>
                             <td>{room.title}</td>
                             <td>{room.price}</td>
                             <td className="text-center">{room.maxPeople}</td>
                             <td>
-                              <input type="checkbox"></input>
+                              <input
+                                type="checkbox"
+                                name="room"
+                                value={room._id}
+                                onChange={handleChange}
+                              ></input>
                             </td>
                           </tr>
                         ))}
@@ -189,9 +296,11 @@ const AddHotel = () => {
                   <div className="col-12 mt-2 d-flex justify-content-center">
                     <button
                       onClick={handleAddHotel}
-                      className="addHotel__button  button button--green"
+                      className={`addHotel__button button ${
+                        isInputsValid ? `button--green` : `button--red`
+                      } `}
                     >
-                      Button
+                      {isInputsValid ? "Send" : "Some Input Invalid"}
                     </button>
                   </div>
                 </div>
